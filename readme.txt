f50b2bb34b5749c9614f9e0c000f3eaeb9f01c6c
Iqbal Hossain <iqbal.212015026@green.ac.bd>
2024-08-13 20:59:31 +0600

    Initial commit

--- readme.txt
+++ readme.txt
@@ -2,7 +2,7 @@
 Contributors:      @iqbal1hossain
 Tags:              block
 Tested up to:      6.1
-Stable tag:        0.0.1
+Stable tag:        1.0.0
 License:           GPL-2.0-or-later
 License URI:       https://www.gnu.org/licenses/gpl-2.0.html
 
--- smart-heading.php
+++ smart-heading.php
@@ -5,7 +5,7 @@
  * Description:       Smart Heading is unique
  * Requires at least: 6.1
  * Requires PHP:      7.0
- * Version:           0.0.1
+ * Version:           1.0.0
  * Author:            @iqbal1hossain
  * License:           GPL-2.0-or-later
  * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
@@ -18,6 +18,43 @@
 if ( ! defined( 'ABSPATH' ) ) {
 	exit; // Exit if accessed directly.
 }
+
+/**
+ * Defining plugin constants.
+ *
+ * @since 1.0.0
+ */
+define('SMART_HEADING_PLUGIN_URL', plugin_dir_url(__FILE__));
+define('SMART_HEADING_PLUGIN_DIR', plugin_dir_path(__FILE__));
+
+
+/**
+ * Defining plugin version
+ *
+ * @since 1.0.0
+ */
+class Smart_Heading_Version {
+	const PLUGIN_VERSION = '1.0.0';
+
+	public static function get_plugin_version() {
+		return self::PLUGIN_VERSION;
+	}
+}
+
+/**
+ * Loads the plugin text domain for the Gutenkit Blocks Addon.
+ *
+ * This function is responsible for loading the translation files for the plugin.
+ * It sets the text domain to 'smart-heading' and specifies the directory
+ * where the translation files are located.
+ *
+ * @param string $domain   The text domain for the plugin.
+ * @param bool   $network  Whether the plugin is network activated.
+ * @param string $directory The directory where the translation files are located.
+ * @return bool True on success, false on failure.
+ * @since 1.0.0
+ */
+load_plugin_textdomain( 'smart-heading', false, SMART_HEADING_PLUGIN_DIR . 'languages/' );
 
 /**
  * Registers the block using the metadata loaded from the `block.json` file.
